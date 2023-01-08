d4f26f9b11e13fab029aab9a17f534ecf9179dd9
aquietone <quietguy466@gmail.com>
2023-01-07 20:28:43 -0800

    update tooltips on enc options

--- aqo/classes/classbase.lua
+++ aqo/classes/classbase.lua
@@ -103,7 +103,7 @@ end
 
 local casterpriests = {clr=true,shm=true,dru=true,mag=true,nec=true,enc=true,wiz=true}
 local petclasses = {nec=true,enc=true,mag=true,bst=true,shm=true,dru=true,shd=true}
-local buffclasses = {clr=true,dru=true,enc=true,shm=true,mag=true,rng=true,bst=true}
+local buffclasses = {clr=true,dru=true,enc=true,shm=true,mag=true,rng=true,bst=true,nec=true}
 local healers = {clr=true,dru=true,shm=true}
 base.addCommonOptions = function()
     if base.SPELLSETS then
